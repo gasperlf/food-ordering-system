67ff53bacde844895d3fe7305d875069ada2c288
candidate-mNiNDlhMDY <gasper_lf@hotmail.com>
2026-02-28 14:13:44 +0100

    kafka

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -0,0 +1,46 @@
+package com.food.ordering.system.kafka.producer.service.impl;
+
+import com.food.ordering.system.kafka.producer.exception.KafkaProducerException;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaProducerImpl <K extends Serializable, V extends SpecificRecordBase> implements KafkaProducer<K, V> {
+
+    private final KafkaTemplate<K, V> kafkaTemplate;
+
+    @Override
+    public void send(String topicName, K key, V message) {
+
+        log.info("Sending message {} to topic {} with key {}",message, topicName, key);
+        CompletableFuture<SendResult<K, V>> send = kafkaTemplate.send(topicName, key, message);
+        send.thenAccept(result -> {
+            RecordMetadata recordMetadata = result.getRecordMetadata();
+            log.info("Message sent successfully to topic {} partition {} offset {} with key {}",
+                    message,recordMetadata.partition(),
+                    recordMetadata.offset(), topicName);
+        }).exceptionally(ex->{
+            throw new KafkaProducerException("Failed to send message with key " + key + " and message " + message);
+        });
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            log.info("Closing kafka template");
+            kafkaTemplate.destroy();
+        }
+    }
+}
